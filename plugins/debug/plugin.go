1076cd21a521eb72a6bc59c91b44e9eb0bb03042
muXxer <mux3r@web.de>
2021-04-25 21:49:52 +0200

    Add free memory debug API call

--- plugins/debug/plugin.go
+++ plugins/debug/plugin.go
@@ -30,8 +30,14 @@ const (
 	RouteDebugComputeWhiteFlag = "/whiteflag"
 
 	// RouteDebugSolidifier is the debug route to manually trigger the solidifier.
-	// GET triggers the solidifier.
-	RouteDebugSolidifier = "/solidifier"
+	// PUT triggers the solidifier.
+	RouteDebugControlSolidifier = "/control/solidifier"
+
+	// RouteDebugControlFreeMemory is the debug route to manually trigger to free unused memory
+	// bound in object storage, requester, and message processor.
+	// If no flags are given, all memories are cleared.
+	// PUT frees the memory.
+	RouteDebugControlFreeMemory = "/control/free-memory"
 
 	// RouteDebugOutputs is the debug route for getting all output IDs.
 	// GET returns the outputIDs for all outputs.
@@ -88,12 +94,13 @@ var (
 
 type dependencies struct {
 	dig.In
-	Storage      *storage.Storage
-	Tangle       *tangle.Tangle
-	RequestQueue gossip.RequestQueue
-	UTXO         *utxo.Manager
-	NodeConfig   *configuration.Configuration `name:"nodeConfig"`
-	Echo         *echo.Echo
+	Storage          *storage.Storage
+	Tangle           *tangle.Tangle
+	RequestQueue     gossip.RequestQueue
+	MessageProcessor *gossip.MessageProcessor
+	UTXO             *utxo.Manager
+	NodeConfig       *configuration.Configuration `name:"nodeConfig"`
+	Echo             *echo.Echo
 }
 
 func configure() {
@@ -111,10 +118,19 @@ func configure() {
 		return restapi.JSONResponse(c, http.StatusOK, resp)
 	})
 
-	routeGroup.GET(RouteDebugSolidifier, func(c echo.Context) error {
+	routeGroup.PUT(RouteDebugControlSolidifier, func(c echo.Context) error {
 		deps.Tangle.TriggerSolidifier()
 
-		return restapi.JSONResponse(c, http.StatusOK, "solidifier triggered")
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	routeGroup.PUT(RouteDebugControlFreeMemory, func(c echo.Context) error {
+		err := controlFreeMemory(c)
+		if err != nil {
+			return nil
+		}
+
+		return c.NoContent(http.StatusNoContent)
 	})
 
 	routeGroup.GET(RouteDebugOutputs, func(c echo.Context) error {
